ff5effcd17c72fe8af28c2589cd159df29065046
Andre <andreluis.alap@gmail.com>
2020-12-09 15:20:38 -0300

    refactoring schedule

--- README.md
+++ README.md
@@ -35,13 +35,7 @@ O negócio está crescendo, e agora ele precisa de um sistema para controlar as 
   *  Fazer testes BDD passarem
   *  Arrumar estética visual com bootstrap
 
-- Problemas a serem resolvidos:
 
- Deixei comentado uma linha no teste de schedule que verifica se o objeto é valido. Criei uma validação na classe Schedule que verifica se as datas estão disponiveis (check_dates). A finalidade deste metodo é passar para o metodo availability, da classe CalendarDay, a data inicial e os dias com a finalidade deste metodo poder verificar se as datas estao disponiveis.
- Porém, passando pelas iterações (com o uso do byebug deixado comentado) as datas são realmente incrementadas, passando no exemplo por exemplo: dia 08, depois dia 09, depois dia 10, mas ai volta para o dia 08. Assim, faz com que o metodo find_by encontre a data e volte false para a verificação de availability. Surgindo assim a duvida de por que a data retorna ao inicio e por que ela encontra a data no banco, tendo em vista que o banco de testes é reiniciado a cada teste e não sendo um metodo que salva e sim procura a data apenas.
- Este erro está ainda à ser analisado com a cabeça mais fresca e corrigido, seguindo o princípio de refatoração, pois faz parte do TDD,utilizado para o exercicio proposto. 
-
- No mais, está com todos os testes passando, cumprindo seus objetivos de dar segurança e de que tudo funciona como deveria funcionar.
 
 
 
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,8 @@
+# Classe que agenda data
 class Schedule < ApplicationRecord
   has_many :calendar_days
 
-  #validations
+  # validations
   validates :initial_date, presence: true
   validates :number_of_days, presence: true
   validates :number_of_days, numericality: { greater_than: 0, only_integer: true }
@@ -10,26 +11,25 @@ class Schedule < ApplicationRecord
   validate :date_cannot_be_in_the_past
 
   def generate_dates
-    date = self.initial_date
-    
-    self.number_of_days.times do
-      calendar_days.new(my_date:date).save
+    date = initial_date
+    number_of_days.times do
+      calendar_days.new(my_date: date).save
       date += 1
     end
   end
 
   def check_dates
-    if CalendarDay.availability(self.initial_date,self.number_of_days)
+    if CalendarDay.availability(initial_date, number_of_days)
       true
     else
-      errors.add(:initial_date, "unavailable dates")
+      errors.add(:initial_date, 'unavailable dates')
       false
     end
   end
 
-  #Método que valida se a data é inferior a Hoje ou igual a hoje
+  # Metodo que valida se a data é inferior a Hoje ou igual a hoje
   def date_cannot_be_in_the_past
-    if self.initial_date.present? && self.initial_date < Date.current + 1         #Pensei em um cenario que não faz muito sentido alugar no mesmo dia, por isso o +1
+    if initial_date.present? && initial_date < Date.current + 1         # Pensei em um cenario que não faz muito sentido alugar no mesmo dia, por isso o +1
       errors.add(:initial_date, "can't be in the past")
     end
   end
